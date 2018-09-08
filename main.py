6417cf3bfe106795073e5f47c934ce0fb0fcae17
Liam Fleet <liamdavid.fleet@gmail.com>
2018-09-08 14:55:52 +0100

    Imports 25 random original words to use as wordgrid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Entry point to the game
 from components.gameboard import gameboard
+import random
 
 ERROR = -1
 RED = 1
@@ -7,16 +8,41 @@ BLUE = 2
 ASSASSIN = 3
 CIV = 4
 
+
 def main():
 	"""
 		Start a human vs human game
 	"""
-	wordgrid = list(map(chr, range(97, 122)))
+	wordfile = "components/srcWords.txt"
+
+	wordgrid = createWord(wordfile)
 	newGame = gameboard(wordgrid)
 
 	print(newGame.currentBoard())
 
 	takeTurn(BLUE, newGame)
+
+def createWord(wordfile):
+	"""
+		Draw 25 random words to use in the game
+	"""
+	wordIndeces = random.sample(range(1, 400), 25)
+	wordIndeces.sort()
+	wordgrid = []
+
+	j = 0
+	with open(wordfile) as fp:
+		for i, line in enumerate(fp):
+			if j > 24:
+				break
+			elif i == wordIndeces[j]:
+				wordgrid.append(line[:-1])
+				j += 1
+			
+	return wordgrid
+
+
+
 
 def takeTurn(team, currentGame):
 	"""
@@ -36,12 +62,10 @@ def takeTurn(team, currentGame):
 
 	print('The codeword is: ', codeword)
 
-	for i in range(1,10):
+	for i in range(1,guessnum):
 
 		outcome = currentGame.checkWord(input('\nPlease type your guess: '))
 
-		if :
-			pass
 		
 
 	
