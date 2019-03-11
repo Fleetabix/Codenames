90e98b54c45935997410de9d4afe0eedfec48907
Liam Fleet <liamfleet.alternate@gmail.com>
2019-03-11 14:49:10 +0000

    Fixed some regex

--- components/generator.py
+++ components/generator.py
@@ -31,8 +31,13 @@ class Gen:
 		for wrd in words:
 			if re.search("^" + wrd, clue.lower()):
 				return False
-			if re.search("^" + clue.lower(), wrd):
-				return False
+				try:
+					if re.search("^" + clue.lower(), wrd):
+						return False
+					
+				except Exception as e:
+					print(clue)
+					raise e
 	
 		return True
 
@@ -82,6 +87,21 @@ class Human_Gen(Gen):
 		return (codeword, guessnum)
 
 
+
+
+class Chaos_Gen(Gen):
+	"""
+	Just returns blank clues with maximum word count - maximum unhelpfulness
+	"""
+	def __init__(self, team):
+		super().__init__(team)
+		
+	def give_clue(self, currentGame):
+		"""
+		Gives an empty clue, but highest possible number of words
+		"""
+		return ("", currentGame.wordCount(self.team))
+
 class Automated_Gen(Gen):
 	"""
 	docstring for Automated_Generator
@@ -101,34 +121,22 @@ class Automated_Gen(Gen):
 		"""
 		if (self.team == RED):
 			results = self.model.most_similar(
-				positive = currentGame.redWords[:3],
+				positive = currentGame.redWords,
 				negative = [currentGame.assassinWord]
 			)
 
 		else:
 			results = self.model.most_similar(
-				positive = currentGame.blueWords[:3],
+				positive = currentGame.blueWords,
 				negative = [currentGame.assassinWord]
 			)
 
 		for word in results:
 			if self.valid_clue(currentGame.wordgrid, word[0]):
-				return (word[0], 3)
-
-
-class Chaos_Gen(Gen):
-	"""
-	Just returns blank clues with maximum word count - maximum unhelpfulness
-	"""
-	def __init__(self, team):
-		super().__init__(team)
-		
-	def give_clue(self, currentGame):
-		"""
-		Gives an empty clue, but highest possible number of words
-		"""
-		return ("", currentGame.wordCount(self.team))
-
+				guessnum = 3
+				if currentGame.wordCount(self.team) < 3:
+					guessnum = currentGame.wordCount(self.team)
+				return (word[0], guessnum)
 	
 class Wiki_Gen(Gen):
 
@@ -144,17 +152,67 @@ class Wiki_Gen(Gen):
 
 		if (self.team == RED):
 			results = self.model.most_similar(
-				positive = currentGame.redWords[:3],
+				positive = currentGame.redWords,
 				negative = [currentGame.assassinWord]
 			)
 
 		else:
 			results = self.model.most_similar(
-				positive = currentGame.blueWords[:3],
+				positive = currentGame.blueWords,
 				negative = [currentGame.assassinWord]
 			)
 
 		for word in results:
 			if self.valid_clue(currentGame.wordgrid, word[0].rstrip(')')):
-				return (word[0].rstrip(')'), 3)
+				guessnum = 3
+				if currentGame.wordCount(self.team) < 3:
+					guessnum = currentGame.wordCount(self.team)
+				return (word[0].rstrip(')'), guessnum)
 
+class Strategic_Gen(Gen):
+
+	def __init__(self, team):
+
+		super().__init__(team)
+		self.model = gensim.models.KeyedVectors.load_word2vec_format(
+			'components/models/GoogleNews-vectors-negative300.bin', 
+			binary=True, 
+			limit=100000
+			)
+
+	def give_clue(self, currentGame):
+		"""
+			Returns the codeword and number of words to guess - always 3
+		"""
+
+		remaining_words = currentGame.wordCount(self.team)
+
+		if (self.team == RED):
+
+			if remaining_words < 5:
+				results = self.model.most_similar(
+					positive = currentGame.redWords,
+					negative = [currentGame.assassinWord]
+				)
+			else: 
+				for i in range(0, remaining_words - 4):
+					pass
+
+
+
+
+
+
+
+		else:
+			results = self.model.most_similar(
+				positive = currentGame.blueWords,
+				negative = [currentGame.assassinWord]
+			)
+
+		for word in results:
+			if self.valid_clue(currentGame.wordgrid, word[0]):
+				guessnum = 4
+				if remaining_words < 4:
+					guessnum = remaining_words
+				return (word[0], guessnum)
