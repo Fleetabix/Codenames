dcf8855f29236078681ee090c4647b88feb6fdbc
Liam Fleet <liamfleet.alternate@gmail.com>
2019-03-11 08:56:44 +0000

    Added wiki gen, added regex for DPEDIA matches, increased word size to 10000

--- components/generator.py
+++ components/generator.py
@@ -1,26 +1,32 @@
 from .gameboard import gameboard
 import gensim
 import abc
+import re
+
+ERROR = -1
+RED = 1
+BLUE = 2
+ASSASSIN = 3
+CIV = 4
 
 class Gen:
 	"""A general guesser class"""
 
 	__metaclass__ = abc.ABCMeta
 
-	ERROR = -1
-	RED = 1
-	BLUE = 2
-	ASSASSIN = 3
-	CIV = 4
+	
 
-
-
+	
 	def __init__(self, team):
+		
 		self.team = team
 
 	@staticmethod
-	def valid_guess(words, guess):
-		return True
+	def valid_clue(words, clue):
+		if re.search("^DBPEDIA", clue) :
+			return False
+		else:
+			return True
 
 	@abc.abstractmethod
 	def give_clue(self, currentGame):
@@ -39,6 +45,7 @@ class Human_Gen(Gen):
 		"""
 			Returns the codeword and a number of words to guess
 		"""
+		input('\nPass to guesser\n')
 
 		print(currentGame.currentBoard())
 		
@@ -46,7 +53,7 @@ class Human_Gen(Gen):
 
 			codeword = input('\nPlease type your codeword: ')
 
-			if self.valid_guess(currentGame.wordgrid, codeword) :
+			if self.valid_clue(currentGame.wordgrid, codeword) :
 				break
 			else :
 				print("Not a valid codeword!")
@@ -73,8 +80,12 @@ class Automated_Gen(Gen):
 	"""
 	def __init__(self, team):
 
-		self.super().__init__(team)
-		self.model = gensim.models.KeyedVectors.load_word2vec_format('models/GoogleNews-vectors-negative300.bin', binary=True, limit=50000)
+		super().__init__(team)
+		self.model = gensim.models.KeyedVectors.load_word2vec_format(
+			'components/models/GoogleNews-vectors-negative300.bin', 
+			binary=True, 
+			limit=100000
+			)
 
 	def give_clue(self, currentGame):
 		"""
@@ -83,17 +94,17 @@ class Automated_Gen(Gen):
 		if (self.team == RED):
 			results = self.model.most_similar(
 				positive = currentGame.redWords[:3],
-				negative = currentGame.assassinWord
+				negative = [currentGame.assassinWord]
 			)
 
 		else:
 			results = self.model.most_similar(
 				positive = currentGame.blueWords[:3],
-				negative = currentGame.assassinWord
+				negative = [currentGame.assassinWord]
 			)
 
 		for word in results:
-			if self.valid_guess(currentGame.wordgrid, word[0]):
+			if self.valid_clue(currentGame.wordgrid, word[0]):
 				return (word[0], 3)
 
 
@@ -108,6 +119,34 @@ class Chaos_Gen(Gen):
 		"""
 		Gives an empty clue, but highest possible number of words
 		"""
-		return ("", currentGame.wordCount(team))
+		return ("", currentGame.wordCount(self.team))
 
-		
+	
+class Wiki_Gen(Gen):
+
+	def __init__(self, team):
+		super().__init__(team)
+		self.model = gensim.models.KeyedVectors.load_word2vec_format(
+			'components/models/wiki_model.bin', 
+			binary=True,
+			limit=100000
+			)
+
+	def give_clue(self, currentGame):
+
+		if (self.team == RED):
+			results = self.model.most_similar(
+				positive = currentGame.redWords[:3],
+				negative = [currentGame.assassinWord]
+			)
+
+		else:
+			results = self.model.most_similar(
+				positive = currentGame.blueWords[:3],
+				negative = [currentGame.assassinWord]
+			)
+
+		for word in results:
+			if self.valid_clue(currentGame.wordgrid, word[0]):
+				return (word[0], 3)
+
